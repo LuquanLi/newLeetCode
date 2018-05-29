e75157cac08406ea1eee6fa8580d762d9e4a7e10
Luquan Li <lli@US02112-ml.corp.qc>
2018-05-29 11:05:12 -0700

    05/29 -2

--- src/array_others.java
+++ src/array_others.java
@@ -32,4 +32,32 @@ public class array_others {
         result[0] = 1;
         return result;
     }
+
+    // 43
+    public String multiply(String num1, String num2) {
+        // input validation
+        if (num1 == null || num2 == null) {
+            return null;
+        }
+
+        // digit m * digit n, result has most m + n digits
+        int[] result = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >=0; i --) {
+            for (int j = num2.length() - 1; j >=0; j --) {
+                result[i + j + 1] = result[i + j + 1] + (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                result[i + j] = result[i + j] + result[i + j + 1] / 10;
+                result[i + j + 1] = result[i + j + 1] % 10;
+            }
+        }
+
+        // remove leading 0
+        StringBuilder sb = new StringBuilder();
+        boolean nonZero = false;
+        for (int i = 0; i < result.length; i ++) {
+            if (result[i] != 0) nonZero = true;
+            if (nonZero) sb.append(result[i]);
+            if (i == result.length - 1 && !nonZero) return "0"; // all 0 in result array
+        }
+        return sb.toString();
+    }
 }
